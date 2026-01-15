ac77331708cd67c653198381e6e311769ecfef8d
Pratik Kamble <pratik.kamble@shwaira.com>
2026-01-15 14:12:47 +0530

    consistent file naming

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print(1+1)
+# Everything in python is Object
+#     eg. Number, Boolean
+#     Code:
+#         age = 2 // reference to address of 2 in memory
+#         age = 12 // when you update value it does not not update value of va, it change reference of age var to address of 12 in memory
+#         name = 2 // same referencing  to address of 2 in memory ie age referencing
+
+# age = 2
+# print(age)
+# age = 12
+# print(age)
+
+print(id(2),"2")
+print(id(1),"1")
+# print(id(age), "age")
+# print(id(12), "12")
+
+# name=2
+# print(id(name), "name")
+# print(id(2), "2")
+# print(id(age), "age")
